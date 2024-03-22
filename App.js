aa52f6b587e4397d5537b3943ba24c750e91adf9
Zimbo Sebastião <94145573+ZimboSebastiao@users.noreply.github.com>
2024-03-22 08:54:38 -0300

    configurações iniciais

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar>
+        <View style={estilos.container}>
+          <Button title="Escolher foto" />
+          <Image style={{ width: 300, height: 300 }} />
+        </View>
+      </StatusBar>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
