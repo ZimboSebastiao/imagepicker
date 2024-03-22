9aa7fdba187cb9346ce656acc873f466623e8765
Zimbo Sebastião <94145573+ZimboSebastiao@users.noreply.github.com>
2024-03-22 09:50:06 -0300

    configurações finais

--- App.js
+++ App.js
@@ -1,15 +1,50 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View, Image } from "react-native";
+import { useState, useEffect } from "react";
+
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
+  // State tradicional para armazenar a referência da foto (quando existir)
+  const [foto, setFoto] = useState(null);
+
+  // State de checagem de permissões de uso (através do hook useCameraPermission)
+  const [status, requestPermission] = ImagePicker.useCameraPermissions();
+  console.log(status);
+
+  useEffect(() => {
+    async function verificaPermissoes() {
+      const camerastatus = await ImagePicker.requestCameraPermissionsAsync();
+      requestPermission(camerastatus === "granted");
+    }
+    verificaPermissoes();
+  }, []);
+
+  const EscolherFoto = async () => {
+    const resultado = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: false,
+      aspect: [16, 9],
+      quality: 1,
+    });
+
+    if (!resultado.canceled) {
+      setFoto(resultado.assets[0].uri);
+    }
+  };
+  console.log(foto);
+
   return (
     <>
-      <StatusBar>
-        <View style={estilos.container}>
-          <Button title="Escolher foto" />
-          <Image style={{ width: 300, height: 300 }} />
-        </View>
-      </StatusBar>
+      <StatusBar style="auto" />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Button title="Escolher foto" onPress={EscolherFoto} />
+        {foto ? (
+          <Image source={{ uri: foto }} style={{ width: 300, height: 300 }} />
+        ) : (
+          <Text>Você ainda não escolheu uma foto</Text>
+        )}
+      </View>
     </>
   );
 }
@@ -17,7 +52,7 @@ export default function App() {
 const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "black",
     alignItems: "center",
     justifyContent: "center",
   },
